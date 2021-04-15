7a2d38434dbc06b0009c482d32d1f31eb17f19ce
gen.ryongfa <ryonghwa.om@gmail.com>
2021-04-15 19:58:07 +0000

    cloudinary

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,12 @@ class ImageUploader < CarrierWave::Uploader::Base
   process resize_to_fit: [400, 200]
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  if Rails.env.production?
+    include Cloudinary::CarrierWave
+  else
+    storage :file
+  end
+  
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
